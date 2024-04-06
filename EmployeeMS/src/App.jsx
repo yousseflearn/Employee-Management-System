13cf0a3d7d45cf190bfd88946e01981bbcef84a0
youssef learn <youssefeasylearn@gmail.com>
2024-04-06 13:32:48 -0700

    Create Employee MS and Login Page

---
+++ EmployeeMS/src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ EmployeeMS/src/components/Login.jsx
@@ -0,0 +1,47 @@
+import "./style.css";
+const Login = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
+      <div className="p-3 rounded w-25 border loginForm">
+        <h2 className="text-center">Login Page</h2>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email:</strong>
+            </label>
+            <input
+              type="email"
+              name="email"
+              autoComplete="off"
+              placeholder="Enter Email"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password:</strong>
+            </label>
+            <input
+              type="password"
+              name="password"
+              autoComplete="off"
+              placeholder="Enter password"
+              className="form-control rounded-0"
+            />
+          </div>
+          <button className="btn btn-success w-100 rounded-0 mb-2">
+            Log In
+          </button>
+          <div className="mb-1">
+            <input type="checkbox" name="tick" id="tick" className="me-2" />
+            <label htmlFor="password">
+              You are agree with terms & conditions
+            </label>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
