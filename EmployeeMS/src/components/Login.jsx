6cafcc2b01d35f8ed8df09ab9115e77da2e37b68
youssef learn <youssefeasylearn@gmail.com>
2024-04-08 15:54:54 -0700

    Handle Input Fields  and Pass Data to server(mysql db and JWT token)

--- EmployeeMS/src/components/Login.jsx
+++ EmployeeMS/src/components/Login.jsx
@@ -1,10 +1,26 @@
 import "./style.css";
+import { useState } from "react";
+import axios from "axios";
+
 const Login = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3000/auth/adminlogin", values)
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
       <div className="p-3 rounded w-25 border loginForm">
         <h2 className="text-center">Login Page</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="email">
               <strong>Email:</strong>
@@ -15,6 +31,9 @@ const Login = () => {
               autoComplete="off"
               placeholder="Enter Email"
               className="form-control rounded-0"
+              onChange={(e) => {
+                setValues({ ...values, email: e.target.value });
+              }}
             />
           </div>
           <div className="mb-3">
@@ -27,6 +46,9 @@ const Login = () => {
               autoComplete="off"
               placeholder="Enter password"
               className="form-control rounded-0"
+              onChange={(e) => {
+                setValues({ ...values, password: e.target.value });
+              }}
             />
           </div>
           <button className="btn btn-success w-100 rounded-0 mb-2">
@@ -43,5 +65,4 @@ const Login = () => {
     </div>
   );
 };
-
 export default Login;
--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,19 @@
 import express from "express";
+import { adminRouter } from "./Routes/AdminRoute.js";
+import cors from "cors";
+
 const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT"],
+    credentials: true,
+  })
+);
+app.use("/auth", adminRouter);
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
